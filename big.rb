d67095bff4c8583ef5e83356e4d1cac462d64e4b
femto <femtowin@gmail.com>
2020-08-30 20:20:00 +0800

    initial

---
+++ big.rb
@@ -0,0 +1,239 @@
+class InvalidBigDecimalException < Exception
+  def initialize(big_decimal_str, reason)
+    super("Invalid BigDecimal: #{big_decimal_str} (#{reason})")
+  end
+end
+
+class DivisionByZeroError < Exception
+  def initialize()
+    super("Unhandled exception: Division by 0")
+  end
+end
+
+class Big < Numeric
+  ZERO = 0
+  TEN = 10
+  DEFAULT_MAX_DIV_ITERATIONS = 100
+  attr_reader :value, :scale
+
+  def self.new(num, scale = nil)
+    if num.is_a?(Float)
+      return new(num.to_s)
+    end
+    if num.is_a?(Integer) && scale.nil?
+      return new(num, 0)
+    end
+    if num.is_a?(self)
+      return num
+    end
+    super
+
+  end
+
+  def initialize(num, scale = nil)
+    if scale
+      @value = num.to_i #should we call num.to_i ?
+      @scale = scale
+    end
+    if num.is_a?(String)
+      str = num
+      str = str.delete_prefix('+')
+      str = str.delete("_")
+      raise InvalidBigDecimalException.new(str, "Zero size") if str.bytesize == 0
+
+      # Check str's validity and find index of '.'
+      decimal_index = nil
+      # Check str's validity and find index of 'e'
+      exponent_index = nil
+
+      str.each_char.with_index do |char, index|
+        case char
+        when '-'
+          unless index == 0 || exponent_index == index - 1
+            raise InvalidBigDecimalException.new(str, "Unexpected '-' character")
+          end
+        when '+'
+          unless exponent_index == index - 1
+            raise InvalidBigDecimalException.new(str, "Unexpected '+' character")
+          end
+        when '.'
+          if decimal_index
+            raise InvalidBigDecimalException.new(str, "Unexpected '.' character")
+          end
+          decimal_index = index
+        when 'e', 'E'
+          if exponent_index
+            raise InvalidBigDecimalException.new(str, "Unexpected #{char.inspect} character")
+          end
+          exponent_index = index
+        when '0'..'9'
+          # Pass
+        else
+          raise InvalidBigDecimalException.new(str, "Unexpected #{char.inspect} character")
+        end
+      end
+
+      decimal_end_index = (exponent_index || str.size) - 1
+      if decimal_index
+        decimal_count = (decimal_end_index - decimal_index)
+
+        io = StringIO.new
+        # We know this is ASCII, so we can slice by index
+        io << (str[0, decimal_index])
+        io << (str[decimal_index + 1, decimal_count])
+        @value = io.string.to_i
+      else
+        decimal_count = 0
+        @value = str[0..decimal_end_index].to_i
+      end
+
+      if exponent_index
+        exponent_postfix = str[exponent_index + 1]
+        case exponent_postfix
+        when '+', '-'
+          exponent_positive = exponent_postfix == '+'
+          exponent = str[(exponent_index + 2)..-1].to_i
+        else
+          exponent_positive = true
+          exponent = str[(exponent_index + 1)..-1].to_i
+        end
+
+        @scale = exponent
+        if exponent_positive
+          if @scale < decimal_count
+            @scale = decimal_count - @scale
+          else
+            @scale -= decimal_count
+            @value *= 10 ** @scale
+            @scale = 0
+          end
+        else
+          @scale += decimal_count
+        end
+      else
+        @scale = decimal_count
+      end
+
+    end
+  end
+
+  def -
+    self.class.new(-@value, @scale)
+  end
+
+  def +(other)
+    other = self.class.new(other)
+    if @scale > other.scale
+      scaled = other.scale_to(self)
+      Big.new(@value + scaled.value, @scale)
+    elsif @scale < other.scale
+      scaled = scale_to(other)
+      Big.new(scaled.value + other.value, other.scale)
+    else
+      Big.new(@value + other.value, @scale)
+    end
+  end
+
+  def -(other)
+    other = self.class.new(other)
+    if @scale > other.scale
+      scaled = other.scale_to(self)
+      Big.new(@value - scaled.value, @scale)
+    elsif @scale < other.scale
+      scaled = scale_to(other)
+      Big.new(scaled.value - other.value, other.scale)
+    else
+      Big.new(@value - other.value, @scale)
+    end
+  end
+
+  def *(other)
+    other = self.class.new(other)
+    Big.new(@value * other.value, @scale + other.scale)
+  end
+
+  def /(other)
+    other = self.class.new(other)
+    div other
+  end
+
+  def div(other, max_div_iterations = DEFAULT_MAX_DIV_ITERATIONS)
+    check_division_by_zero other
+    other.factor_powers_of_ten
+
+    scale = @scale - other.scale
+    numerator, denominator = @value, other.value
+
+    quotient, remainder = numerator.divmod(denominator)
+
+    if remainder == ZERO
+      return Big.new(normalize_quotient(other, quotient), scale)
+    end
+
+    remainder = remainder * TEN
+    i = 0
+    while remainder != ZERO && i < max_div_iterations
+      inner_quotient, inner_remainder = remainder.divmod(denominator)
+      quotient = quotient * TEN + inner_quotient
+      remainder = inner_remainder * TEN
+      i += 1
+    end
+
+    Big.new(normalize_quotient(other, quotient), scale + i)
+  end
+
+  def <=>(other )
+    other = self.class.new(other)
+    if @scale > other.scale
+      @value <=> other.scale_to(self).value
+    elsif @scale < other.scale
+      scale_to(other).value <=> other.value
+    else
+      @value <=> other.value
+    end
+  end
+
+  def scale_to(new_scale)
+    in_scale(new_scale.scale)
+  end
+
+  def in_scale(new_scale)
+    if @value == 0
+      Big.new(0, new_scale)
+    elsif @scale > new_scale
+      scale_diff = @scale - new_scale
+      Big.new(@value / power_ten_to(scale_diff), new_scale)
+    elsif @scale < new_scale
+      scale_diff = new_scale - @scale
+      Big.new(@value * power_ten_to(scale_diff), new_scale)
+    else
+      self
+    end
+  end
+
+  def normalize_quotient(other, quotient)
+    if (@value < 0 && other.value > 0) || (other.value < 0 && @value > 0)
+      -quotient.abs
+    else
+      quotient
+    end
+  end
+
+  private def check_division_by_zero(bd)
+    raise DivisionByZeroError.new if bd.value == 0
+  end
+
+  private def power_ten_to(x)
+    TEN ** x
+  end
+
+  protected def factor_powers_of_ten
+    while @scale > 0
+      quotient, remainder = value.divmod(TEN)
+      break if remainder != 0
+
+      @value = quotient
+      @scale = @scale - 1
+    end
+  end
+end
---
+++ big_test.rb
@@ -0,0 +1,154 @@
+require 'minitest/autorun'
+require './big'
+# 注意有些资料中，测试类不是继承自MiniTest::Test，
+# 那是MiniTest 5之前的做法，MiniTest会通知你改正
+class TestMyLife < MiniTest::Test
+  # 这个方法会在各个测试之前被调用
+  def setup
+    #@big = Big.new("0.00")
+  end
+
+  def test_build_from_string
+    # big1 = Big.new("0.00")
+    # assert_equal 0, big1.value
+    # assert_equal 0, big1.scale
+
+    big2 = Big.new("3.")
+    assert_equal 3, big2.value
+    assert_equal 0, big2.scale
+
+    big3 = Big.new("3")
+    assert_equal 3, big3.value
+    assert_equal 0, big3.scale
+  end
+
+  def test_build_from_string_exponent
+    # big1 = Big.new("0.00")
+    # assert_equal 0, big1.value
+    # assert_equal 0, big1.scale
+
+    big2 = Big.new("3.e1")
+    assert_equal 30, big2.value
+    assert_equal 0, big2.scale
+
+    # big3 = Big.new("3")
+    # assert_equal 3, big3.value
+    # assert_equal 0, big3.scale
+  end
+
+  def test_add
+    # big2 = Big.new("3.e1")
+    # assert_equal 30, big2.value
+    # assert_equal 0, big2.scale
+    #
+     big3 = Big.new("3")
+     assert_equal 3, big3.value
+     assert_equal 0, big3.scale
+    #
+    # big4 = big2+big3
+    # assert_equal 33, big4.value
+    # assert_equal 0, big4.scale
+
+    big2 = Big.new(3,1)
+    big4 = big2+big3
+    assert_equal 33, big4.value
+    assert_equal 1, big4.scale
+  end
+
+  def test_sub
+    # big2 = Big.new("3.e1")
+    # assert_equal 30, big2.value
+    # assert_equal 0, big2.scale
+    #
+    big3 = Big.new("3")
+    assert_equal 3, big3.value
+    assert_equal 0, big3.scale
+    #
+    # big4 = big2+big3
+    # assert_equal 33, big4.value
+    # assert_equal 0, big4.scale
+
+    big2 = Big.new(4,1)
+    big4 = big3-big2
+    assert_equal 26, big4.value
+    assert_equal 1, big4.scale
+  end
+
+  def test_mul
+    # big2 = Big.new("3.e1")
+    # assert_equal 30, big2.value
+    # assert_equal 0, big2.scale
+    #
+    big3 = Big.new("0.3")
+    assert_equal 3, big3.value
+    assert_equal 1, big3.scale
+    #
+    # big4 = big2+big3
+    # assert_equal 33, big4.value
+    # assert_equal 0, big4.scale
+
+    big2 = Big.new(4,1)
+    big4 = big3*big2
+    assert_equal 12, big4.value
+    assert_equal 2, big4.scale
+  end
+
+  def test_div
+    # big2 = Big.new("3.e1")
+    # assert_equal 30, big2.value
+    # assert_equal 0, big2.scale
+    #
+    big3 = Big.new("0.3")
+    assert_equal 3, big3.value
+    assert_equal 1, big3.scale
+
+    big0 = Big.new("0.0")
+    assert_equal 0, big0.value
+    assert_equal 1, big0.scale
+
+    assert_raises DivisionByZeroError do
+      big3/big0
+    end
+
+    #
+    # big4 = big2+big3
+    # assert_equal 33, big4.value
+    # assert_equal 0, big4.scale
+
+    big2 = Big.new(4,1)
+    big4 = big3/big2
+    assert_equal 75, big4.value
+    assert_equal 2, big4.scale
+
+    big = Big.new(1).div(Big.new(2))
+    assert_equal 5, big.value
+    assert_equal 1, big.scale
+
+    big = Big.new(1).div(Big.new(3), 5)
+
+    assert_equal 33333, big.value
+    assert_equal 5, big.scale
+  end
+
+  def test_compare
+    big3 = Big.new("0.3")
+    assert_equal 3, big3.value
+    assert_equal 1, big3.scale
+
+    big4 = Big.new("4.2")
+    assert_equal 42, big4.value
+    assert_equal 1, big4.scale
+
+    result = big3 <=> big4
+
+    assert_equal -1, result
+  end
+
+  def test_sleep
+    # assert_equal exp, act, msg
+    #assert_equal   "zzZ", @big.sleep, "I don't sleep well "
+  end
+
+  def teardown
+  end
+end
